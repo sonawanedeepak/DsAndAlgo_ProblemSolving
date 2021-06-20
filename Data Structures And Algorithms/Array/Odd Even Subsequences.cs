f940b57e6721ab34bdbe54e18d0f1cdf5def6ad0
Deepak Sonawane <sonawanedeepak944@gmail.com>
2021-06-20 11:25:54 +0530

    HomeWork

---
+++ Data Structures And Algorithms/Array/Odd Even Subsequences.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithms.Array
+{
+    class Odd_Even_Subsequences
+    {
+        //Odd Even Subsequences
+        //Given an array of integers A of size N.Find the longest subsequence of A which is odd-even.
+
+        //A subsequence is said to odd-even in the following cases:
+
+        //The first element of the subsequence is odd, second element is even, third element is odd and so on. For example: [5, 10, 5, 2, 1, 4], [1, 2, 3, 4, 5]
+
+        //The first element of the subsequence is even, second element is odd, third element is even and so on. For example: [10, 5, 2, 1, 4, 7], [10, 1, 2, 3, 4]
+
+        //Return the maximum possible length of odd-even subsequence.
+
+        //Note: An array B is a subsequence of an array A if B can be obtained from A by deletion of several (possibly, zero or all) elements.
+
+
+        //Input Format
+
+        //The only argument given is the integer array A.
+        //Output Format
+
+        //Return the maximum possible length of odd-even subsequence.
+        //Constraints
+
+        //1 <= N <= 100000
+        //1 <= A[i] <= 10^9 
+        //For Example
+
+        //Input 1:
+        //    A = [1, 2, 2, 5, 6]
+        //Output 1:
+        //    4
+        //    Explanation 1:
+        //        Maximum length odd even subsequence is [1, 2, 5, 6]
+
+        //Input 2:
+        //    A = [2, 2, 2, 2, 2, 2]
+        //Output 2:
+        //    1
+        //    Explanation 2:
+        //        Maximum length odd even subsequence is [2]
+
+        public int solve(List<int> A)
+        {
+            bool even = A[0] % 2 == 0 ? true : false;
+            int cntr = 0;
+            for (int i = 1; i <= A.Count - 1; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    if (even)
+                    {
+                        cntr++;
+                    }
+                    even = true;
+                }
+                else
+                {
+                    if (!even)
+                    {
+                        cntr++;
+                    }
+                    even = false;
+                }
+
+            }
+            return A.Count - cntr;
+
+        }
+    }
+}
---
+++ Data Structures And Algorithms/Array/Time to equality.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithms.Array
+{
+    class Time_to_equality
+    {
+        //Time to equality
+        //Problem Description
+
+        //Given an integer array A of size N.In one second you can increase the value of one element by 1.
+
+        //Find the minimum time in seconds to make all elements of the array equal.
+
+
+        //Problem Constraints
+        //1 <= N <= 1000000
+        //1 <= A[i] <= 1000
+
+
+        //Input Format
+        //First argument is an integer array A.
+
+
+        //Output Format
+        //Return an integer denoting the minimum time to make all elements equal.
+
+
+        //Example Input
+        //A = [2, 4, 1, 3, 2]
+
+
+        //Example Output
+        //8
+
+
+        //Example Explanation
+        //We can change the array A = [4, 4, 4, 4, 4].The time required will be 8 seconds.
+
+        public int solve(List<int> A)
+        {
+            int highest = 0;
+            int sum = 0;
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (A[i] > highest)
+                {
+                    highest = A[i];
+                }
+            }
+
+            for (int i = 0; i < A.Count; i++)
+            {
+
+                sum += highest - A[i];
+
+            }
+            return sum;
+        }
+    }
+}
